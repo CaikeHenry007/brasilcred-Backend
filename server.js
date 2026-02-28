19a297e7e0550660c4792d0455d38031eed3d20d
CaikeHenry007 <caikehenry12@gmail.com>
2026-02-28 20:02:39 -0300

    correçao

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const leadRoutes = require("./routes/learoutes");
+const leadRoutes = require("./routes/leadroutes");
 const adminRoutes = require("./routes/adminroutes");
 
 const app = express();

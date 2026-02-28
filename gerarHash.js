abb3e343b999c594d6871265795fed7af4bfbba9
CaikeHenry007 <caikehenry12@gmail.com>
2026-02-28 19:10:14 -0300

    backend pronto

--- gerarHash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt");
-
-const senha = "Brasilcred@2026"; // coloque a senha real do admin aqui
-
-bcrypt.hash(senha, 10).then((hash) => {
-  console.log("HASH GERADO:");
-  console.log(hash);
-});

4ae1469a59da67ac344c7d0cc6c5853c73b71f8c
CaikeHenry007 <caikehenry12@gmail.com>
2026-03-01 19:42:14 -0300

    correção de bugs

--- controllers/leadcontroller.js
+++ controllers/leadcontroller.js
@@ -10,3 +10,40 @@ exports.getLeads = async (req, res) => {
     res.status(500).json({ message: "Erro ao buscar leads" });
   }
 };
+
+exports.createLead = async (req, res) => {
+  try {
+    const {
+      nome,
+      whatsapp,
+      email,
+      cpf,
+      faz_parte_bolsa_familia,
+      recebe_caixa_tem,
+      recebe_400_ou_mais,
+      possui_emprestimo_crefisa,
+    } = req.body;
+
+    const result = await pool.query(
+      `INSERT INTO leads 
+      (nome, whatsapp, email, cpf, faz_parte_bolsa_familia, recebe_caixa_tem, recebe_400_ou_mais, possui_emprestimo_crefisa) 
+      VALUES ($1,$2,$3,$4,$5,$6,$7,$8) 
+      RETURNING *`,
+      [
+        nome,
+        whatsapp,
+        email,
+        cpf,
+        faz_parte_bolsa_familia,
+        recebe_caixa_tem,
+        recebe_400_ou_mais,
+        possui_emprestimo_crefisa,
+      ],
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao salvar lead" });
+  }
+};
--- routes/leadroutes.js
+++ routes/leadroutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const leadController = require("../controllers/leadcontroller");
 const authMiddleware = require("../middleware/authmiddleware");
 
+router.post("/", leadController.createLead);
 router.get("/", authMiddleware, leadController.getLeads);
 
 module.exports = router;
